f92b0c822f6c3242e0e466e67f44c9b1e186d383
Seb Jones <seb.jones@enovate.co.uk>
2021-12-30 17:52:43 +0000

    Try to generate wildcard certs in setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+mv /usr/local/etc/dnsmasq.conf /usr/local/etc/dnsmasq.conf.bak
+mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.bak
+
 ln -s dnsmasq.conf /usr/local/etc/dnsmasq.conf
+ln -s nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
+ln -s nginx/snippets /usr/local/etc/nginx/snippets
+
+# Set up wildcard self-signed certificate for *.test domain
+mkcert -install
+
+mkcert -cert-file /usr/local/etc/ca-certificates/_wildcard.test.pem \
+       -key-file  /usr/local/etc/ca-certificates/_wildcard.test-key.pem \
+       "*.test"

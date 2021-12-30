7e706a9a9c4f1fcd20257994a20812db823cf75e
Seb Jones <seb.jones@enovate.co.uk>
2021-12-30 18:03:21 +0000

    Add php symlink and some echos to setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
+echo "Renaming existing files to backups"
+
 mv /usr/local/etc/dnsmasq.conf /usr/local/etc/dnsmasq.conf.bak
 mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.bak
+mv /usr/local/bin/php /usr/local/bin/php.bak
+
+echo "Creating symlinks to new files"
 
 ln -s dnsmasq.conf /usr/local/etc/dnsmasq.conf
 ln -s nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
 ln -s nginx/snippets /usr/local/etc/nginx/snippets
+ln -s php.sh /usr/local/bin/php
 
-# Set up wildcard self-signed certificate for *.test domain
+echo "Creating wildcard self-signed certificate for *.test domain"
+
 mkcert -install
 
 mkcert -cert-file /usr/local/etc/ca-certificates/_wildcard.test.pem \

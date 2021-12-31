6ddf720048014368ed241917bd70a39970d51845
Seb Jones <jones.seb95@googlemail.com>
2021-12-31 19:41:32 +0000

    Store test command output as artifact

--- test.sh
+++ test.sh
@@ -13,19 +13,23 @@ brew services restart php@7.3
 brew services restart php@8.0
 brew services restart nginx
 
-# Create directories for test sites
+# Create directories for test sites and output
 mkdir -pv ~/sites/p73/php73-test/public
 mkdir -pv ~/sites/p80/php80-test/public
+mkdir -pv ~/test-output
 
 # Add test files to site directories
 echo '<?php phpinfo(); ?>' > ~/sites/p73/php73-test/public/index.php
 echo '<?php phpinfo(); ?>' > ~/sites/p80/php80-test/public/index.php
 
 # Try running each PHP file in the terminal and check for correct version number
-cd ~/sites/p73/php73-test/public && php index.php | grep -E '^PHP\s+Version\s+=>\s+7.3'
-cd ~/sites/p80/php80-test/public && php index.php | grep -E '^PHP\s+Version\s+=>\s+8.0'
+cd ~/sites/p73/php73-test/public && php index.php > ~/test-output/php73-test-output.txt \
+    && grep -E '^PHP\s+Version\s+=>\s+7.3' ~/test-output/php73-test-output.txt
+cd ~/sites/p80/php80-test/public && php index.php > ~/test-output/php80-test-output.txt \
+    && grep -E '^PHP\s+Version\s+=>\s+8.0' ~/test-output/php80-test-output.txt
 
 # Send a HTTP request to each site and check version of result
-mkdir -pv ~/test-output
-curl --verbose --insecure 'https://php73-test.test' -o ~/test-output/php73-test-output.html && grep -E 'PHP\s+Version\s+7.3' ~/test-output/php73-test-output.html
-curl --verbose --insecure 'https://php80-test.test' -o ~/test-output/php80-test-output.html && grep -E 'PHP\s+Version\s+8.0' ~/test-output/php80-test-output.html
+curl --verbose --insecure 'https://php73-test.test' -o ~/test-output/php73-test-output.html \
+    && grep -E 'PHP\s+Version\s+7.3' ~/test-output/php73-test-output.html
+curl --verbose --insecure 'https://php80-test.test' -o ~/test-output/php80-test-output.html \
+    && grep -E 'PHP\s+Version\s+8.0' ~/test-output/php80-test-output.html
